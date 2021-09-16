cc5f3dc076d96a9c2f85575d10a5eaa1a8dbf4c3
mtaranto <marcio.taranto@gmail.com>
2021-09-16 09:52:48 -0300

    Encerra Aula de JavaScript ES6 Avançado

---
+++ 1-debugging.js
@@ -0,0 +1,51 @@
+
+// class app {
+//   constructor() {
+//     this.bind();
+//   }
+
+//   bind() {
+//     document.querySelector('.add-button').addEventListener('click', () => {
+//     this.handleClick(1);
+//   });
+
+//     document.querySelector('.sub-button').addEventListener('click', () => {
+//     this.handleClick(-1);
+//   });
+// }
+
+//   handleClick(val) {
+//     const conterEl = document.querySelector('.counter');
+//     const value = parseInt(conterEl.innerText);
+
+//     conterEl.innerText = value + val;
+//   }
+// }
+
+// new app();
+
+// Console
+/*
+console.log('Black test');
+console.warn('Yellow text with alert');
+console.error('Red error text');
+
+console.trace();
+
+
+console.group('My group');
+console.log('Info inside group');
+console.log('More info inside group');
+console.groupEnd('My group');
+*/
+
+/*
+console.time('Log time');
+setTimeout(() => {
+  console.timeEnd('Log time');
+}, 2000);
+*/
+
+// console.table(['Márcio Taranto', 'Digital Innovation One']);
+// console.assert(1===1, 'Ops');
+// console.log('%c styled log', 'color: blue; font-size: 40px');
